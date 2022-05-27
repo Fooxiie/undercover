79fc55087f0ced2cecf6beede295f1d3395dd179
Bastien <b.ranc@c3rb.fr>
2022-05-27 16:41:27 +0200

    Event chat message

---
+++ app/Events/NewChatMessage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewChatMessage implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $id;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($id, $message)
+    {
+        $this->id = $id;
+        $this->message = $message;
+    }
+
+    public function broadcastOn()
+    {
+        return ['my-channel'];
+    }
+
+    public function broadcastAs()
+    {
+        return 'newChatMessage';
+    }
+}
