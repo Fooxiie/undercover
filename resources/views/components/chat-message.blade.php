6fd59e1fee17a9ea0e8da5f9a41eb199b9c8fba3
Fooxiie <dev.fooxiie@gmail.com>
2022-05-29 01:12:10 +0200

    Utilisation de l'avatar discord dans le chat

--- resources/views/components/chat-message.blade.php
+++ resources/views/components/chat-message.blade.php
@@ -1,6 +1,6 @@
 <div class="flex bg-gray-600 p-1 w-full h-auto min-h-12 mb-0.5 relative">
     <img class="inline-block h-full w-10 rounded-full ring-2 ring-white"
-         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+         src="{{\Illuminate\Support\Facades\Auth::user()->discord_avatar}}"
          alt="avatar">
     <div class="text-white m-2 flex">
         <div class="ml-1 mr-6" style="word-wrap: normal"><span class="font-bold">{{$pseudo}}</span>{{$message}}</div>
