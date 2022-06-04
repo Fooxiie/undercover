97c2d95b12b459536b806d57ed034aaeb1126c51
Fooxiie <dev.fooxiie@gmail.com>
2022-06-04 03:31:39 +0200

    Fix rapide du copier coller

--- resources/views/onGame.blade.php
+++ resources/views/onGame.blade.php
@@ -60,18 +60,17 @@
 
                 <!--Footer-->
                 <div class="flex justify-end pt-2">
-                    <div class="tooltip-reverse">
+                    <div class="tooltip">
                         <button class="px-4 bg-transparent p-3 rounded-lg
                     text-plot hover:bg-plot hover:text-black
                     mr-2" id="copyBtn" onclick="myFunction()"
                                 onmouseout="outFunc()">
                             <span class="tooltiptext" id="myTooltip">Copy to clipboard</span>
                             <svg xmlns="http://www.w3.org/2000/svg"
-                                 class="h-full w-8" viewBox="0 0 20 20"
+                                 class="h-5 w-5" viewBox="0 0 20 20"
                                  fill="currentColor">
-                                <path d="M8 2a1 1 0 000 2h2a1 1 0 100-2H8z"/>
                                 <path
-                                    d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z"/>
+                                    d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"/>
                             </svg>
                         </button>
                     </div>
